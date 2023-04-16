92ae00c97f133d7f807e0b3b8d1a794145fb9c23
Danil Korneev <d26112018@yandex.ru>
2023-04-16 21:32:49 +0300

    2

--- les_2/task_1.java
+++ les_2/task_1.java
@@ -1,39 +1,31 @@
 
-
 // Реализуйте алгоритм сортировки пузырьком числового массива, результат после каждой итерации запишите в лог-файл.
 
 import java.util.Arrays;
-import java.util.logging.*;
 import java.io.IOException;
-
+import java.io.FileWriter;
 
 public class task_1 {
-
-
     public static void main(String[] args) throws IOException {
-        Logger logger = Logger.getLogger(task_1.class.getName());
-        FileHandler fh = new FileHandler("log.txt");
-        logger.addHandler(fh);
-        SimpleFormatter sFormat = new SimpleFormatter();
-        fh.setFormatter(sFormat);
-
 
         int[] arrayTemp = {2, 5, 1, -7, 3, 64, 0};
         System.out.println(Arrays.toString(arrayTemp));
-        for (int out = arrayTemp.length - 1; out >= 1; out--) {
-            for (int i = 0; i < out; i++) {
-                if(arrayTemp[i] > arrayTemp[i+1]) {
-                    int temp = arrayTemp[i];
-                    arrayTemp[i] = arrayTemp[i+1];
-                    arrayTemp[i+1] = temp;
+
+        try (FileWriter fw = new FileWriter("log.txt")) {
+            for (int out = arrayTemp.length - 1; out >= 1; out--) {
+                for (int i = 0; i < out; i++) {
+                    if(arrayTemp[i] > arrayTemp[i+1]) {
+                        int temp = arrayTemp[i];
+                        arrayTemp[i] = arrayTemp[i+1];
+                        arrayTemp[i+1] = temp;
+                    }
                 }
+                fw.append(Arrays.toString(arrayTemp)).append("\n");
                 
             }
-            logger.info(Arrays.toString(arrayTemp));
-        }
-        System.out.println(Arrays.toString(arrayTemp));
-
-
-
+            System.out.println(Arrays.toString(arrayTemp));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }    
     }
 }
---
+++ les_3/task1.java
@@ -0,0 +1,13 @@
+package les_3;
+
+public class task1 {
+    public static void main(String[] args) {
+        
+        Object o = 1; GetType(o); 
+    }
+
+    static void GetType(Object obj) {
+        System.out.println(obj.getClass().getName());
+        }
+       
+}
